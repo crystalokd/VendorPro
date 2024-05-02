6c8f90c05f48bbffc260ba95112b1fb7119dd630
crystal okedi <68944153+crystalokd@users.noreply.github.com>
2024-05-02 10:01:36 +0100

    Update shortcuts.py

--- vendorpro/utils/shortcuts.py
+++ vendorpro/utils/shortcuts.py
@@ -9,17 +9,3 @@ def get_random_code():
     """
     return str(uuid.uuid4()).split("-")[-1]
 
-# from .models import Vendor, Order
-
-
-def calculate_otd():
-    total_orders = Order.objects.count()
-    on_time_orders = Order.objects.filter(delivered_on_time=True).count()
-
-    if total_orders == 0:
-        otd_percentage = 0
-    else:
-        otd_percentage = (on_time_orders / total_orders) * 100
-
-    # Update the OTD percentage for all vendors
-    Vendor.objects.update(on_time_delivery_rate=otd_percentage)
